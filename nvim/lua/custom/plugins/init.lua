c0c12ab9fca7eb70ecc81559be5c026181dc05a5
Colm Rafferty <colmjr@outlook.com>
2026-05-21 10:57:25 +0800

    hahaha added smear cursor, so buttery smooth(probably not good for productivity but lol

--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -9,9 +9,7 @@ local function gh(repo) return 'https://github.com/' .. repo end
 local function pack_add(specs, opts)
   local original_wait = vim.wait
   vim.wait = function(timeout, callback, interval, fast_only)
-    if timeout == 30000 then
-      timeout = vim.g.pack_install_timeout_ms or 120000
-    end
+    if timeout == 30000 then timeout = vim.g.pack_install_timeout_ms or 120000 end
     return original_wait(timeout, callback, interval, fast_only)
   end
   local ok, err = pcall(vim.pack.add, specs, opts)
@@ -19,11 +17,21 @@ local function pack_add(specs, opts)
   if not ok then error(err, 2) end
 end
 
--- pack_add { gh 'NeogitOrg/neogit' }
+-- - pack_add { gh 'NeogitOrg/neogit' }
 -- require('neogit').setup {}
 --
 -- vim.keymap.set('n', '<leader>gg', '<cmd>Neogit<CR>', { desc = 'Neogit' })
 pack_add { gh 'tpope/vim-fugitive' }
+pack_add { gh 'sphamba/smear-cursor.nvim' }
+require('smear_cursor').setup {
+  stiffness = 0.9,
+  trailing_stiffness = 0.5,
+  distance_stop_animating = 0.5,
+  smear_between_buffers = true,
+  smear_between_neighbor_lines = true,
+  legacy_computing_symbols_support = true,
+  smear_insert_mode = true,
+}
 
 local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
 for file_name, type in vim.fs.dir(plugins_dir) do
