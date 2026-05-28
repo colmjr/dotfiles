8a9a67ef4541c65d74c30a1bf78b0e4371edd546
Colm Rafferty <colmjr@outlook.com>
2026-05-28 19:17:26 +0800

    added (not working for tmux yet) integration for tmux

--- nvim/lua/colmjr/remap.lua
+++ nvim/lua/colmjr/remap.lua
@@ -23,8 +23,4 @@ vim.keymap.set({ 'n', 'v' }, '<leader>y', [["+y]])
 vim.keymap.set({ 'n', 'v' }, '<leader>Y', [["+Y]])
 vim.keymap.set({ 'n', 'v' }, '<leader>d', [["_d]])
 
--- wincmd magic
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+-- wincmd magic (handled by vim-tmux-navigator)
