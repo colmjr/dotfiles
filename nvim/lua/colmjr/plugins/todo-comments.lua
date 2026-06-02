a7ea1012ac68da411cc70d1a0cb55b640ba66ebe
Colm Rafferty <colmjr@outlook.com>
2026-06-02 20:23:11 +0800

    added todo telescope and longer which key waiting time

--- nvim/lua/colmjr/plugins/todo-comments.lua
+++ nvim/lua/colmjr/plugins/todo-comments.lua
@@ -1,2 +1,3 @@
 vim.pack.add { 'https://github.com/folke/todo-comments.nvim' }
 require('todo-comments').setup()
+vim.keymap.set('n', '<leader>st', '<cmd>TodoTelescope<CR>', { desc = '[S]earch [T]odo' })
