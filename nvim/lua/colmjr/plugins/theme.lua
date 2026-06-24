c8edb2f447d4e5b273afe9f63f308916d79e4707
Colm Rafferty <colmjr@outlook.com>
2026-06-24 16:59:52 +0800

    changed all to rose pine

--- nvim/lua/colmjr/plugins/theme.lua
+++ nvim/lua/colmjr/plugins/theme.lua
@@ -1,18 +1,18 @@
-vim.pack.add { 'https://github.com/rebelot/kanagawa.nvim' }
-require('kanagawa').setup {
-  theme = 'wave', -- Load "wave" theme
-  background = { -- map the value of 'background' option to a theme
-    dark = 'wave', -- try "dragon" !
-    light = 'lotus',
-  },
-}
-vim.cmd 'colorscheme kanagawa'
-
--- vim.pack.add {
---   {
---     src = 'https://github.com/rose-pine/neovim',
---     name = 'rose-pine',
+-- vim.pack.add { 'https://github.com/rebelot/kanagawa.nvim' }
+-- require('kanagawa').setup {
+--   theme = 'wave', -- Load "wave" theme
+--   background = { -- map the value of 'background' option to a theme
+--     dark = 'wave', -- try "dragon" !
+--     light = 'lotus',
 --   },
 -- }
--- require('rose-pine').setup()
--- vim.cmd 'colorscheme rose-pine'
+-- vim.cmd 'colorscheme kanagawa'
+
+vim.pack.add {
+  {
+    src = 'https://github.com/rose-pine/neovim',
+    name = 'rose-pine',
+  },
+}
+require('rose-pine').setup()
+vim.cmd 'colorscheme rose-pine'
